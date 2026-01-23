290d2113dcc19cdd5e0d1261292f5036e70c5878
Sourav Singh <singhsourav0110@gmail.com>
2026-01-24 02:35:31 +0530

    Added correct base url

--- src/constant/appConstant.tsx
+++ src/constant/appConstant.tsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001/";
+export const BASE_URL = "https://api.shivaliwashingcompany.in/";
 
 // export const BASE_URL = "http://localhost:3001/"; // for local development
 
